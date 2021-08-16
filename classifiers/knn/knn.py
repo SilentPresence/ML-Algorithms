88b3726fb94463574023936f3fcda39f6cfc2b64
Maxim <SilentPresence@users.noreply.github.com>
2021-08-16 19:35:25 +0300

    Added distance parameter and input casting

--- classifiers/knn/knn.py
+++ classifiers/knn/knn.py
@@ -1,25 +1,43 @@
 from sklearn.base import BaseEstimator,ClassifierMixin
 import numpy as np
-from sklearn.metrics.pairwise import euclidean_distances
 from scipy import stats
+from scipy.spatial.distance import cdist
+import pandas as pd
 
 class kNNClassifier(BaseEstimator, ClassifierMixin):
-  def __init__(self, n_neighbors):
+  def __init__(self, n_neighbors,distance='euclidean'):
     self.n_neighbors = n_neighbors
+    self.distance=distance
 
   def fit(self, X, y):
+    if isinstance(X, list):
+      X=np.array(X)
+    elif isinstance(X, pd.DataFrame):
+      X=X.values
+    if isinstance(y, list):
+      y=np.array(y)
+    elif isinstance(y, pd.DataFrame):
+      y=y.values
+    elif isinstance(y, pd.Series):
+      y=y.values
+
     self._X=X
     self._y=y
     return self
 
   def predict(self, X,method='brute'):
+    if isinstance(X, list):
+      X=np.array(X)
+    elif isinstance(X, pd.DataFrame):
+      X=X.values
+
     if method=='brute':
       return self.__predict__(X)
     #TODO: add kdetree
     return self.__predict__(X)
 
   def __predict__(self,X):
-    distance_matrix=euclidean_distances(self._X,X)
+    distance_matrix=cdist(self._X,X,metric=self.distance)
 
     prediction=np.empty(X.shape[0])
     for i in range(X.shape[0]):
